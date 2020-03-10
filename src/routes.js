9090affa240e5c771439d419df644aae7566123a
Guilherme <guilherme@ludium.com.br>
2020-03-10 20:42:56 -0300

    listagem de prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/Multer'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,8 +14,12 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions',SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files',upload.single('file'),FileController.store);
 
