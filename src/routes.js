d55a47a0e5f9a1ec64ae08c8c9fc2146b95245bd
Shima <guilherme.shima91@hotmail.com>
2020-03-19 13:42:07 -0300

    adição de migration, model, controller e alteração no arquivo de rota

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
-import multerConfig from './config/Multer'
+// eslint-disable-next-line import/no-unresolved
+import multerConfig from './config/Multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +22,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files',upload.single('file'),FileController.store);
 
