20b23d857cd367dc8154a940255783f631c91750
Shima <guilherme.shima91@hotmail.com>
2020-03-30 13:20:23 -0300

    tentativa de resolução do erro

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,23 +1,24 @@
 import jwt from 'jsonwebtoken';
-import { promisify} from 'util';
+import { promisify } from 'util';
+
 import authConfig from '../../config/auth';
 
-export default async(req,res,next) => {
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if(!authHeader){
-    return res.status(401).json({error:'Token not provided'});
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
-  const [,token] = authHeader.split(' ');
+  const [, token] = authHeader.split(' '); // descarta o primeiro elemento do array (bearer)
 
   try {
-    const decoded = await promisify(jwt.verify)(token,authConfig.secret);
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    req.userId = decoded.id;
+    req.userId = decoded.id; // inclui o usuário na request
 
     return next();
-  }catch(err){
-    return res.status(401).json({error:'Token invalid'});
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid.' });
   }
-}
+};
--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -3,13 +3,14 @@ import Sequelize, { Model } from 'sequelize';
 class Appointment extends Model {
   static init (sequelize){
     super.init(
-    {
-      date: Sequelize.DATE,
-      canceled_at: Sequelize.DATE,
-    },
-    {
-      sequelize,
-    });
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
 
     return this;
   }
