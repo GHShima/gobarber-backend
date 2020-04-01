8edd0b426a38508867ee283d72fc8062a8b98ffc
Shima <guilherme.shima91@hotmail.com>
2020-04-01 01:10:14 -0300

    atualização de dependencias

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -83,6 +83,12 @@ class AppointmentController {
         .json({ error: 'Appointment date is not available' });
     }
 
+    if (req.userId === provider_id) {
+      return res
+        .status(401)
+        .json({ error: 'Impossible to schedule a appointment with yourself' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
