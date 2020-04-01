335982be7ac4e57b89eb5d8a00b3caedccbd1a4e
Shima <guilherme.shima91@hotmail.com>
2020-04-01 10:24:20 -0300

    .env bug fix

--- src/database/index.js
+++ src/database/index.js
@@ -24,7 +24,7 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(process.env.MONG_URL, {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useFindAndModify: true,
       useUnifiedTopology: true,
