df3f48881a7b50375353adf438e1266b85e4d778
Shima <guilherme.shima91@hotmail.com>
2020-04-01 06:15:54 -0300

    config, lib e envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'aa04cff13fc630',
+    pass: 'b81ca56f9aab31',
+  },
+  default: {
+    from: 'Equipe GoBarber<teste@teste.com>',
+  },
+};
